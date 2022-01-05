88534243823605d7a343230e108b17c9029fc381
Ogechi Ike <ogechiike@admins-mbp.fm.pri>
2022-01-05 12:49:11 -0600

    finally found a solution to /[media]/[id] url info issue

---
+++ next-film/pages/[media]/[id].js
@@ -0,0 +1,26 @@
+import React, {Fragment} from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { getDetailsById } from "../../src/lib/api";
+
+
+export default function Media({details}){
+    console.log(details)
+    return (
+        <Fragment>
+            <Head>
+                <title></title>
+            </Head>
+        </Fragment>
+    )
+}
+
+export async function getServerSideProps({params}){
+    const details = await getDetailsById(params.media, params.id)
+    return{
+        props:{
+            details,
+        }
+    }
+}
